4a149b6874ab19448da404b81be1455902f9a789
jesus ibanez <jibanez@tacitknowledge.com>
2024-05-05 22:25:26 -0700

    removed code from post item component

--- src/features/posts/components/PostItem.tsx
+++ src/features/posts/components/PostItem.tsx
@@ -16,19 +16,7 @@ export const PostItem = ({ postData, setSelectedPost, selectedPost }: any) => (
     data-testid="post-item"
   >
     <Meta title={postData.title} description={postData.body} />
-    <div className="flex justify-between items-center mt-4">
-      <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
-        Read More
-      </button>
-      <div className="flex items-center">
-        <img
-          className="w-8 h-8 rounded-full mr-2"
-          src="https://via.placeholder.com/50"
-          alt="Avatar"
-        />
-        <p className="text-gray-700">John Doe</p>
-      </div>
-    </div>
+    
   </Card>
 );
 
